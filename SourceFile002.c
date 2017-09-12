890c74d996d04d36a933511514d89af2f14edc4e
AurumEtArgentum <31777607+AurumEtArgentum@users.noreply.github.com>
2017-09-12 10:15:42 -0400

    Proposed Code -Julia

--- SourceFile002.c
+++ SourceFile002.c
@@ -1,8 +1,17 @@
 
 task main()
 {
-	// this is a dog;
+	motor(motorleft)= 50;
+	motor(motorright)=50;
 
-
+	// this is not a dog;
+	while(SensorValue(buttonAny) == 1)
+	{
+		motor(motorleft)= -50;
+		motor(motorright)= -50;
+		motor(motorleft)= 50;
+		wait1Msec(2000);
+		motor(motorleft)= -50;
+		motor(motorright)= -50;
 
 }

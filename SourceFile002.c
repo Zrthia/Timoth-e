07c6d3e9c67b23ad157e3c5e523dc35bbee20c53
Zrthia <31777532+Zrthia@users.noreply.github.com>
2017-09-12 10:09:34 -0400

    random thing that i decided to add

--- SourceFile002.c
+++ SourceFile002.c
@@ -1,9 +1,17 @@
 
 task main()
 {
-// this is a dog;
-// you are pigeon;
-
-
+	// this is a dog;
+	// you are pigeon;
+	while(buttonAny) = 0;
+		{
+			motor[motorB] = 40;
+			motor[motorC] = 40;
+		}
+	if(buttonAny) = 1
+	{
+		motor[motorB] = -100;
+		motor[motorC] = -100;
+		wait1Msec(2523);
 
 }

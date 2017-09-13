4bb92322fa7e895115b59b297d6ae0ec785348c6
Zrthia <31777532+Zrthia@users.noreply.github.com>
2017-09-13 13:26:30 -0400

    we jabe autism

--- Timoth-e.c
+++ Timoth-e.c
@@ -5,15 +5,17 @@ task main()
 	{
 		motor(motorB)= 50;
 		motor(motorC)= 50;
-		if(SensorValue(buttonAny)= 1)
-		{
-			motor(motorB)= -50;
-			motor(motorC) = -50;
-			wait1Msec(1000);
-			motor(motorB)= 100;
-			motor(motorC) = -100;
-			wait1Msec(500);
-		}
+		wait1Msec(1000);
+			while(SensorValue(buttonAny)== 1)
+			{
+				motor(motorB)= -50;
+				motor(motorC) = -50;
+				wait1Msec(1000);
+			}
+		motor(motorB)= 50;
+		motor(motorC) = 50;
+
+
 	}
 
 

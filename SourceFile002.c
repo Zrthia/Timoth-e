a50c2c69a80a9e666680d733f0eccc6e6f0fdaf8
AurumEtArgentum <31777607+AurumEtArgentum@users.noreply.github.com>
2017-09-13 12:59:59 -0400

    no

--- SourceFile002.c
+++ SourceFile002.c
@@ -2,6 +2,4 @@
 task main()
 {
 
-		wait1Msec(2000);
-		motor(motorC)= -50;
 }
--- Timoth-e.c
+++ Timoth-e.c
@@ -1,5 +1,20 @@
 
 task main()
 {
+	while( true )
+	{
+		motor(motorB)= 50;
+		motor(motorC)= 50;
+		if(SensorValue(buttonAny)= 1)
+		{
+			motor(motorB)= -50;
+			motor(motorC) = -50;
+			wait1Msec(1000);
+			motor(motorB)= 100;
+			motor(motorC) = -100;
+			wait1Msec(500);
+		}
+	}
+
 
 }

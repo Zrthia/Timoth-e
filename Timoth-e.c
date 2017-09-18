fe5ac8a1c266196dd7b3d6cbd980419b19e99792
Zrthia <31777532+Zrthia@users.noreply.github.com>
2017-09-18 11:10:09 -0400

    errything is gone

--- Timoth-e.c
+++ Timoth-e.c
@@ -4,27 +4,5 @@
 
 task main()
 {
-	while(true)
-	{
-		motor(motorB)= 50;
-		motor(motorC)= 50;
-		wait1Msec(1000);
-		if(SensorValue(ButtonRight) == 1)
-		{
-			motor[motorB] = -50;
-			motor[motorC] = -50;
-			wait1Msec(1000);
-			motor[motorB] = 100;
-			motor[motorC] = -100;
-			wait1Msec(595);
-	  }
-
-
-	}
-
-
 
 }
-
-//variables can be used to mark what speeds result in a 90 degree turn or 180 turn
-//if wait1Msec 595 will turn 180 degrees around with -100 and 100

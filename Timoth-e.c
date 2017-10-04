b750aeeaf597cf0884e4c03f2c3ea2bd5fa338eb
AurumEtArgentum <31777607+AurumEtArgentum@users.noreply.github.com>
2017-10-04 10:41:10 -0400

    julia est muppetere

--- Timoth-e.c
+++ Timoth-e.c
@@ -1,76 +1,7 @@
-#pragma config(Sensor, S1,     ButtonLeft,     sensorEV3_Touch)
-#pragma config(Sensor, S4,     ButtonRight,    sensorEV3_Touch)
 
 task main()
 {
-	int speedleft = 0;
-	int speedright = 20;
-	int time = 1000;
 
-
-		while(speedleft <= 80)
-		{
-			motor[motorB] = speedleft + 40;
-			motor[motorC] = speedright + 50;
-			wait1Msec(time);
-
-			speedleft = speedleft + 3;
-			speedright = speedright + 2;
-			time = time + 500;
-		}
-
-
-	while(true)
-	{
-		while((SensorValue(ButtonLeft) == 0 && speedleft <= 100))
-			{
-				motor(motorB) = speedleft + 20;
-				motor(motorC) = speedright + 50;
-				wait1Msec(time);
-
-				speedleft = speedleft + 3;
-				speedright = speedright + 2;
-				time = time + 500;
-			}
-
-		while(SensorValue(ButtonLeft) == 1 || SensorValue(ButtonRight)== 1)
-		{
-			motor(motorB) = -40;
-			motor(motorC) = -70;
-			wait1Msec(1000);
-
-			motor(motorB) = 60;
-			motor(motorC) = 70;
-			wait1Msec(1900);
-			resetMotorEncoder(motorB);
-			resetMotorEncoder(motorC);
-
-			//this is successful!
-
-		}
-
-		speedleft = 0;
-		speedright = 20;
-		time = 1000;
-
-
-		while((getMotorEncoder(motorB) > 5000 && SensorValue(ButtonLeft) == 0 && speedleft <= 80))
-		{
-			//(spiraling)
-			motor(motorB) = speedleft + 40;
-			motor(motorC) = speedright + 50;
-			wait1Msec(time);
-
-			speedleft = speedleft + 3;
-			speedright = speedright + 2;
-			time = time + 500;
-
-		}
-
-
-
-
-	}
 
 
 }
